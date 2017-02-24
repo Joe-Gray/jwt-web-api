63bed1c6c7f0321b3482da24eebaa9b59e168293
Joe-Gray <joseph.gray@carvana.com>
2017-02-24 15:43:13 -0700

    updated database schema for many to many Roles/Claims

--- Carvana.MarketExpansion.WebApi/SqlScript/PopulateSecurity.sql
+++ Carvana.MarketExpansion.WebApi/SqlScript/PopulateSecurity.sql
@@ -17,36 +17,50 @@ END
 SELECT @adminRoleId = SecurityRoleID FROM MarketAuth.tblSecurityRole WHERE Name = 'Admin'
 SELECT @marketRoleId = SecurityRoleID FROM MarketAuth.tblSecurityRole WHERE Name = 'ManageMarket'
 
+
+
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityClaim WHERE Name = 'Admin')
 BEGIN
 	
-	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, SecurityRoleID, RowLoadedDateTime, RowUpdatedDateTime) 
-	VALUES ('978898FD-DF36-442E-B33A-CFA31B325178', 'Admin', @adminRoleId, SYSDATETIME(), SYSDATETIME())
+	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, RowLoadedDateTime, RowUpdatedDateTime) 
+	VALUES ('978898FD-DF36-442E-B33A-CFA31B325178', 'Admin', SYSDATETIME(), SYSDATETIME())
 END
 
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityClaim WHERE Name = 'AddMarket')
 BEGIN
 	
-	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, SecurityRoleID, RowLoadedDateTime, RowUpdatedDateTime) 
-	VALUES ('BA8076A3-6EFA-4CDD-93FF-1156F3456803', 'AddMarket', @marketRoleId, SYSDATETIME(), SYSDATETIME())
+	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, RowLoadedDateTime, RowUpdatedDateTime) 
+	VALUES ('BA8076A3-6EFA-4CDD-93FF-1156F3456803', 'AddMarket', SYSDATETIME(), SYSDATETIME())
 END
 
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityClaim WHERE Name = 'EditMarket')
 BEGIN
-	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, SecurityRoleID, RowLoadedDateTime, RowUpdatedDateTime) 
-	VALUES ('9E4E7F4C-45DA-4602-9682-1635216D989C', 'EditMarket', @marketRoleId, SYSDATETIME(), SYSDATETIME())
+	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, RowLoadedDateTime, RowUpdatedDateTime) 
+	VALUES ('9E4E7F4C-45DA-4602-9682-1635216D989C', 'EditMarket', SYSDATETIME(), SYSDATETIME())
 END
 
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityClaim WHERE Name = 'DeleteMarket')
 BEGIN
-	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, SecurityRoleID, RowLoadedDateTime, RowUpdatedDateTime) 
-	VALUES ('C134E23F-B33F-491D-95F9-929E2D94DA57', 'DeleteMarket', @marketRoleId, SYSDATETIME(), SYSDATETIME())
+	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, RowLoadedDateTime, RowUpdatedDateTime) 
+	VALUES ('C134E23F-B33F-491D-95F9-929E2D94DA57', 'DeleteMarket', SYSDATETIME(), SYSDATETIME())
 END
 
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityClaim WHERE Name = 'ViewMarket')
 BEGIN
-	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, SecurityRoleID, RowLoadedDateTime, RowUpdatedDateTime) 
-	VALUES ('CED9FC91-B19A-42D1-A463-3EB05DE9F502', 'ViewMarket', @marketRoleId, SYSDATETIME(), SYSDATETIME())
+	INSERT INTO MarketAuth.tblSecurityClaim(SecurityClaimGUID, Name, RowLoadedDateTime, RowUpdatedDateTime) 
+	VALUES ('CED9FC91-B19A-42D1-A463-3EB05DE9F502', 'ViewMarket', SYSDATETIME(), SYSDATETIME())
+END
+
+IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityRoleClaim WHERE SecurityRoleId = @adminRoleId)
+BEGIN
+	INSERT INTO MarketAuth.tblSecurityRoleClaim(SecurityRoleClaimGUID, SecurityClaimId, SecurityRoleId, RowLoadedDateTime, RowUpdatedDateTime) 
+	SELECT NEWID(), SecurityClaimId, @adminRoleId, SYSDATETIME(), SYSDATETIME() FROM MarketAuth.tblSecurityClaim
+END
+
+IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityRoleClaim WHERE SecurityRoleId = @marketRoleId)
+BEGIN
+	INSERT INTO MarketAuth.tblSecurityRoleClaim(SecurityRoleClaimGUID, SecurityClaimId, SecurityRoleId, RowLoadedDateTime, RowUpdatedDateTime) 
+	SELECT NEWID(), SecurityClaimId, @marketRoleId, SYSDATETIME(), SYSDATETIME() FROM MarketAuth.tblSecurityClaim WHERE Name IN ('AddMarket', 'EditMarket', 'DeleteMarket', 'ViewMarket')
 END
 
 IF NOT EXISTS(SELECT 1 FROM MarketAuth.tblSecurityUser WHERE Email = 'test@test.com')
